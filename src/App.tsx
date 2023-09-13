2c98f1e4dfff7a3028ad35e83a2463d649d08661
Samson Okunola <smsnmicheal@gmail.com>
2023-09-13 04:38:58 +0100

    fixed play/pause button bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ErrorBoundary } from "react-error-boundary"
 import { AnimatePresence } from "framer-motion"
 import { useSnapshot } from "valtio"
-import { useEffect } from "react"
 
 import { ErrorFallback, Player } from "components"
 import Router from "router"
@@ -9,11 +8,6 @@ import state from "store"
 
 const App = () => {
 	const snap = useSnapshot(state)
-
-	useEffect(() => {
-		window.addEventListener("click", () => (state.music = true))
-		return () => window.removeEventListener("click", () => {})
-	}, [])
 
 	return (
 		<AnimatePresence>

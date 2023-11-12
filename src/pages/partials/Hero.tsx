f95b47d90c796927c78135b88daa910a71a4cdc1
Samson Okunola <smsnmicheal@gmail.com>
2023-11-12 16:21:56 +0100

    mod: - changed hero background pattern

--- src/pages/partials/Hero.tsx
+++ src/pages/partials/Hero.tsx
@@ -1,15 +1,16 @@
 import { Illustration } from "components"
+import "./styles.css"
 
 const Hero = () => {
 	return (
-		<main className="relative h-[84.5vh] w-full lg:h-[81vh]">
+		<section className="relative h-[84.5vh] w-full lg:h-[81vh] hero">
 			<Illustration />
 			<div className="absolute left-0 top-0 grid h-full w-full place-items-center bg-black/5">
 				<p className="mt-80 text-lg font-light uppercase text-primary lg:text-3xl">
 					developer | fullstack | open source
 				</p>
 			</div>
-		</main>
+		</section>
 	)
 }
 
